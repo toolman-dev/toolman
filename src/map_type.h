2e3c40ae668b6ab21d0e0bb065ddd772818fd46b
ty666 <t1997y@vip.qq.com>
2020-10-05 09:09:03 +0800

    code formap

--- src/map_type.h
+++ src/map_type.h
@@ -8,24 +8,24 @@
 
 namespace toolman {
 
-    class MapType : public Type {
-        public:
-            MapType(const std::string& name, PrimitiveType* key_type, Type* value_type_) 
-                :Type(name), key_type_(key_type), value_type_(value_type_) {}
-            
-            MapType(std::string&& name, PrimitiveType* key_type, Type* value_type_) 
-                :Type(std::move(name)), key_type_(key_type), value_type_(value_type_) {}
+class MapType : public Type {
+    public:
+        MapType(const std::string& name, PrimitiveType* key_type, Type* value_type_) 
+            :Type(name), key_type_(key_type), value_type_(value_type_) {}
+        
+        MapType(std::string&& name, PrimitiveType* key_type, Type* value_type_) 
+            :Type(std::move(name)), key_type_(key_type), value_type_(value_type_) {}
 
-            bool is_map() const override { return true; }
+        bool is_map() const override { return true; }
 
-            PrimitiveType* get_key_type() const { return key_type_; };
-            Type* get_value_type() const { return value_type_; };
+        PrimitiveType* get_key_type() const { return key_type_; };
+        Type* get_value_type() const { return value_type_; };
 
-        private:
-            // In toolman, map key must be primitive type.
-            PrimitiveType* key_type_;
-            Type* value_type_;
-    };
+    private:
+        // In toolman, map key must be primitive type.
+        PrimitiveType* key_type_;
+        Type* value_type_;
+};
 
 } // namespace toolman
 #endif // MAP_TYPE_H
