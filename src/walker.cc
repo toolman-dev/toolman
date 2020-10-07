ced276dfb9f4793e0492f245f14e45826ee92154
tw666 <t888y@qq.com>
2020-10-07 11:25:26 +0800

    reactor decl_type func into cc file

--- src/walker.cc
+++ src/walker.cc
@@ -4,4 +4,26 @@
 
 #include "src/walker.h"
 
-namespace toolman {}  // namespace toolman
+namespace toolman {
+
+template <typename NODE, typename DECL_TYPE>
+  void DeclPhaseWalker::decl_type(NODE* node) {
+    auto id_start_token = node->identifierName()->getStart();
+    auto stmt_info = StmtInfo(
+        {id_start_token->getLine(),
+         node->identifierName()->getStop()->getLine()},
+        {id_start_token->getStartIndex(), id_start_token->getStopIndex()},
+        file_);
+    if (auto search =
+            type_scope_.lookup_type(node->identifierName()->getText());
+        search.has_value()) {
+      errors_.emplace_back(DuplicateDeclError(search.value(), stmt_info));
+      return;
+    } else {
+      type_scope_.declare(std::make_shared<DECL_TYPE>(
+          DECL_TYPE(node->identifierName()->getText(), stmt_info,
+                    node->Pub() != nullptr)));
+    }
+  }
+
+}  // namespace toolman
--- src/walker.h
+++ src/walker.h
@@ -8,6 +8,7 @@
 #include <memory>
 #include <optional>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "ToolmanParserBaseListener.h"
@@ -41,24 +42,7 @@ class DeclPhaseWalker final : public ToolmanParserBaseListener {
   std::shared_ptr<std::string> file_;
 
   template <typename NODE, typename DECL_TYPE>
-  void decl_type(NODE* node) {
-    auto id_start_token = node->identifierName()->getStart();
-    auto stmt_info = StmtInfo(
-        {id_start_token->getLine(),
-         node->identifierName()->getStop()->getLine()},
-        {id_start_token->getStartIndex(), id_start_token->getStopIndex()},
-        file_);
-    if (auto search =
-            type_scope_.lookup_type(node->identifierName()->getText());
-        search.has_value()) {
-      errors_.emplace_back(DuplicateDeclError(search.value(), stmt_info));
-      return;
-    } else {
-      type_scope_.declare(std::make_shared<DECL_TYPE>(
-          DECL_TYPE(node->identifierName()->getText(), stmt_info,
-                    node->Pub() != nullptr)));
-    }
-  }
+  void decl_type(NODE* node);
 };
 
 class RefPhaseWalker final : public ToolmanParserBaseListener {};
