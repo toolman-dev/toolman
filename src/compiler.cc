0d305a95d95404c355a7321515958d0cf8c198b5
ty666 <t1997y@vip.qq.com>
2020-10-22 13:54:30 +0800

    remove code to cc file

--- src/compiler.cc
+++ src/compiler.cc
@@ -3,3 +3,40 @@
 // found in the LICENSE file.
 
 #include "compiler.h"
+namespace toolman {
+std::shared_ptr<Module> Compiler::compile_module(const std::string& src_path) {
+  auto source = std::filesystem::path(src_path).lexically_normal();
+  if (source.is_relative()) {
+    source = base_path_ / source;
+  }
+  if (auto it = modules_.find(source); it != modules_.end()) {
+    return it->second;
+  }
+  auto source_ptr = std::make_shared<std::filesystem::path>(source);
+  DEF_PHASE_WALK(source_ptr, this);
+  walker_.walk(&def_phase_walker, tree);
+  auto module = std::make_shared<Module>(
+      def_phase_walker.type_scope(), def_phase_walker.option_scope(),
+      source_ptr, def_phase_walker.get_errors());
+  modules_.emplace(source, module);
+  return module;
+}
+
+CompileResult Compiler::compile(const std::string& src_path) {
+  auto source_ptr = std::make_shared<std::filesystem::path>(
+      std::filesystem::absolute(src_path).lexically_normal());
+  base_path_ = source_ptr->parent_path();
+  DEF_PHASE_WALK(source_ptr, this);
+
+  auto ref_phase_walker =
+      RefPhaseWalker(def_phase_walker.type_scope(),
+                     def_phase_walker.option_scope(), source_ptr);
+
+  walker_.walk(&ref_phase_walker, tree);
+
+  auto errors = def_phase_walker.get_errors();
+  auto ref_phase_errors = ref_phase_walker.get_errors();
+  errors.insert(errors.end(), ref_phase_errors.begin(), ref_phase_errors.end());
+  return CompileResult(ref_phase_walker.get_document(), errors);
+}
+}  // namespace toolman
--- src/compiler.h
+++ src/compiler.h
@@ -76,42 +76,9 @@ class Compiler {
 
   // Use shared_ptr as return value, Convenient to no longer use import class
   // later.
-  std::shared_ptr<Module> compile_module(const std::string& src_path) {
-    auto source = std::filesystem::path(src_path).lexically_normal();
-    if (source.is_relative()) {
-      source = base_path_ / source;
-    }
-    if (auto it = modules_.find(source); it != modules_.end()) {
-      return it->second;
-    }
-    auto source_ptr = std::make_shared<std::filesystem::path>(source);
-    DEF_PHASE_WALK(source_ptr, this);
-    walker_.walk(&def_phase_walker, tree);
-    auto module = std::make_shared<Module>(
-        def_phase_walker.type_scope(), def_phase_walker.option_scope(),
-        source_ptr, def_phase_walker.get_errors());
-    modules_.emplace(source, module);
-    return module;
-  }
+  std::shared_ptr<Module> compile_module(const std::string& src_path);
 
-  CompileResult compile(const std::string& src_path) {
-    auto source_ptr = std::make_shared<std::filesystem::path>(
-        std::filesystem::absolute(src_path).lexically_normal());
-    base_path_ = source_ptr->parent_path();
-    DEF_PHASE_WALK(source_ptr, this);
-
-    auto ref_phase_walker =
-        RefPhaseWalker(def_phase_walker.type_scope(),
-                       def_phase_walker.option_scope(), source_ptr);
-
-    walker_.walk(&ref_phase_walker, tree);
-
-    auto errors = def_phase_walker.get_errors();
-    auto ref_phase_errors = ref_phase_walker.get_errors();
-    errors.insert(errors.end(), ref_phase_errors.begin(),
-                  ref_phase_errors.end());
-    return CompileResult(ref_phase_walker.get_document(), errors);
-  }
+  CompileResult compile(const std::string& src_path);
 
  private:
   antlr4::tree::ParseTreeWalker walker_;
